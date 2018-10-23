0e664d44eb88a6b73b8889938aeabdcdc79ff33c
Li Zhen <lilaoda@qq.com>
2018-10-23 21:20:45 +0800

    李真
    商品详情页——动效实现
    登录注册页——静态完成

--- xiaomi/public/js/details.js
+++ xiaomi/public/js/details.js
@@ -1,3 +1,41 @@
 $(".close").click(function(){
   $(this).parent().parent().hide()
 })
+// 放大镜
+var $big=$(".d_main .d_left .big");
+var $max=$(".d_main .d_left .max");
+var mx=$big.children(":first").attr("src");
+$max.css("backgroundImage",`url(${mx})`);
+$max.css("backgroundSize",`200% 200%`);
+var $mask=$(".d_left .big .mask"),$onbig=$(".d_left .big .onbig");
+var msize=200,smsize=400,max=smsize-msize;
+$onbig.hover(function(){
+  $mask.toggleClass("none");
+  $max.toggleClass("none");
+}).mousemove(function(e){
+  var top=e.offsetY-msize/2;
+  var left=e.offsetX-msize/2;
+  if (top<0){top=0;}else if (top>max){top=max;}
+  if (left<0){left=0;}else if (left>max){left=max;}
+  $mask.css({top,left});
+  $max.css("backgroundPosition",`${-2.5*left}px ${-2.5*top}px`)
+})
+//页面滚动，浮动元素
+$(window).scroll(function(){
+  if ($(window).scrollTop()>150){
+    $(".nav_top").show()
+    .css({top:18,opacity:1})
+  }else{
+    $(".nav_top").hide()
+    .css({top:-10,opacity:0})
+  }
+  if ($(window).scrollTop()<200){
+    $(".over").css({position:"relative",top:10})
+  }
+  if ($(window).scrollTop()>200){
+    $(".over").css({position:"fixed",top:140})
+  }
+  if ($(window).scrollTop()>1000){
+    $(".over").css({position:"relative",top:800})
+  }
+})
